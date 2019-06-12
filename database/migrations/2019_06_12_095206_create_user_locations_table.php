4b5f448a4e4d6178851298236e91ea8a5ef9f658
Davyd Holovii <mirage.present@gmail.com>
2019-06-12 13:31:28 +0300

    [db_structure] Migrations

---
+++ database/migrations/2019_06_12_095206_create_user_locations_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserLocationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_locations', function (Blueprint $table) {
+            $table->increments('id');
+
+            // User relation
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+
+            $table->decimal('latitude');
+            $table->decimal('longitude');
+            $table->timestamp("created_at");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_locations');
+    }
+}
---
+++ database/migrations/2019_06_12_095331_create_cities_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cities', function (Blueprint $table) {
+            $table->increments('id');
+
+            // Region relation
+            $table->unsignedInteger('region_id');
+            $table->foreign('region_id')
+                ->references('id')
+                ->on('regions')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+
+            $table->string('name')->index();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cities');
+    }
+}
