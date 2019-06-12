6b86de39509f15331f1685cbb99624eb4dda47a4
Davyd Holovii <mirage.present@gmail.com>
2019-06-12 14:06:13 +0300

    [db_structure] Factories

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -11,9 +11,33 @@
 |
 */
 
-$factory->define(App\User::class, function (Faker\Generator $faker) {
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+$factory->define(App\Models\User::class, function (Faker\Generator $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->email,
+        'telegram_id'   => $faker->unique()->randomDigit,
+        'username'      => $faker->userName,
+        'first_name'    => $faker->firstName,
+        'lang'          => $faker->languageCode,
+        'created_at'    => $faker->dateTimeBetween('-3 months', '-31 days')->format("Y-m-d H:i:s"),
+        'updated_at'    => $faker->dateTimeBetween('-30 days')->format("Y-m-d H:i:s"),
     ];
+});
+
+$factory->define(App\Models\UserLocation::class, function (Faker\Generator $faker) {
+    return [
+        'user_id'       => factory(App\Models\User::class)->create()->id,
+        'latitude'      => $faker->latitude,
+        'longitude'     => $faker->longitude,
+        'created_at'    => $faker->dateTimeBetween('-10 days'),
+    ];
+});
+
+$factory->define(App\Models\Church::class, function (Faker\Generator $faker) {
+   return [
+       'city_id'    => \App\Models\City::select('id')->inRandomOrder()->take(1)->first()->id,
+       'name'       => $faker->colorName,
+       'address'    => $faker->address,
+       'latitude'   => $faker->latitude,
+       'longitude'  => $faker->longitude,
+   ];
 });
