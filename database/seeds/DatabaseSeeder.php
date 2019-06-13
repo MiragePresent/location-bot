c2eac65f231d030e027fa912828471e4a85c3cf6
Davyd Holovii <mirage.present@gmail.com>
2019-06-13 09:25:19 +0300

    [db_structure] Seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call('UsersTableSeeder');
+         $this->call(RegionsAndCitiesSeeder::class);
+         $this->call(ChurchSeeder::class);
     }
 }
