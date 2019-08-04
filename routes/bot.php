b11cefcd241ebbc66105a6897d24a9aa4da4e6ef
Davyd Holovii <mirage.present@gmail.com>
2019-08-04 09:53:42 +0300

    Changed setting webhook action

--- routes/bot.php
+++ routes/bot.php
@@ -7,10 +7,4 @@
 /** @var $router \Laravel\Lumen\Routing\Router */
 $router->post("/", ['uses' => 'BotController@webHookCallback']);
 
-$router->get('/setwebhook', function (\App\Services\Bot\Bot $bot) {
-    $ngrok = "https://ac5758d1.ngrok.io";
-
-    $response = $bot->getApi()->setWebhook($ngrok . "/bot");
-
-    echo $response;
-});
+$router->get('/setwebhook', ['uses' => 'BotController@setWebHook']);
