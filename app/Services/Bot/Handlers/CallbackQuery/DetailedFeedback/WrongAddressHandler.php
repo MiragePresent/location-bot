23563333b711395c1c533aac61c69c94382d49e8
Davyd Holovii <mirage.present@gmail.com>
2024-05-15 21:51:59 -0400

    Feedback messages

--- app/Services/Bot/Handlers/CallbackQuery/DetailedFeedback/WrongAddressHandler.php
+++ app/Services/Bot/Handlers/CallbackQuery/DetailedFeedback/WrongAddressHandler.php
@@ -2,7 +2,17 @@
 
 namespace App\Services\Bot\Handlers\CallbackQuery\DetailedFeedback;
 
+use App\Services\Bot\Answer\HelpProjectAnswer;
+
 class WrongAddressHandler extends AbstractDetailedFeedbackVoteHandler 
 {
     public const CALLBACK_DATA = "vote_detailed_incorrect_addresses";
+
+    protected function sendNextMessage(\TelegramBot\Api\Types\Chat $chat): void
+    {
+        $support = $this->getBot()->getSupportInfo();
+        $answer = new HelpProjectAnswer($support['channel']['name'], $support['channel']['link']);
+
+        $this->getBot()->sendTo($chat->getId(), $answer);
+    }
 }
